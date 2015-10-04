9c7baa1e19c0e3be0530e03c2b483b8e2c46ac57
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2015-10-04 14:05:18 +0200

    install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+ln -s ~/GitHubProjects/dotfiles/vimrc ~/.vimrc
+ln -s ~/GitHubProjects/dotfiles/zshrc ~/.zshrc
+
+# Pathogen for VIM
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# NerdTree plugin for VIM
+git clone https://github.com/scrooloose/nerdtree ~/.vim/bundle/nerdtree
+
+# JellyBeans Theme for VIM
+mkdir -p ~/.vim/colors/
+git clone https://github.com/nanotech/jellybeans.vim.git ~/GitHubProjects/jellybeans.vim
+cp ~/GitHubProjects/jellybeans.vim/colors/* ~/.vim/colors/
+rm -rf ~/GitHubProjects/jellybeans.vim
+
+# GoLang plugin for VIM
+git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+echo "open vim and type :GoInstallBinaries and :Helptags ~/.vim/bundle/vim-go/doc"
